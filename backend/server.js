f1848dac63b5360e56d12d5567776e82a9e2573d
sbucheer <sara.ab2004@gmail.com>
2026-07-20 12:00:25 +0300

    monolith2

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ function sortedEntries() {
 }
 
 const app = express();
-app.use(express.static(path.join(__dirname, '..', 'frontend')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 const io = new Server(server);
